2114adc1f403607b7c502b930907ae4869ea4e24
SAM <hoanga9k38@gmail.com>
2024-03-31 15:06:27 +0700

    first commit

---
+++ Ex3_week6_NguyenHuuHoang/Ex3_week6_NguyenHuuHoang/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Ex3_week6_NguyenHuuHoang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textn = new System.Windows.Forms.TextBox();
+            this.textSoNguyenTo = new System.Windows.Forms.TextBox();
+            this.textSoChinhPhuong = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "n = ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 102);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Số nguyên tố";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(30, 245);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Số chính phương";
+            // 
+            // textn
+            // 
+            this.textn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textn.Location = new System.Drawing.Point(83, 35);
+            this.textn.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.textn.Multiline = true;
+            this.textn.Name = "textn";
+            this.textn.Size = new System.Drawing.Size(228, 37);
+            this.textn.TabIndex = 3;
+            // 
+            // textSoNguyenTo
+            // 
+            this.textSoNguyenTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textSoNguyenTo.Location = new System.Drawing.Point(34, 133);
+            this.textSoNguyenTo.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.textSoNguyenTo.Multiline = true;
+            this.textSoNguyenTo.Name = "textSoNguyenTo";
+            this.textSoNguyenTo.Size = new System.Drawing.Size(404, 100);
+            this.textSoNguyenTo.TabIndex = 4;
+            // 
+            // textSoChinhPhuong
+            // 
+            this.textSoChinhPhuong.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textSoChinhPhuong.Location = new System.Drawing.Point(34, 277);
+            this.textSoChinhPhuong.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.textSoChinhPhuong.Multiline = true;
+            this.textSoChinhPhuong.Name = "textSoChinhPhuong";
+            this.textSoChinhPhuong.Size = new System.Drawing.Size(642, 161);
+            this.textSoChinhPhuong.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(534, 58);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 46);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Kết quả";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(534, 133);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 46);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "New";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(534, 200);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 46);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Thoát";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 461);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textSoChinhPhuong);
+            this.Controls.Add(this.textSoNguyenTo);
+            this.Controls.Add(this.textn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "Form1";
+            this.Text = "Số nguyên tố, số chính phương";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textn;
+        private System.Windows.Forms.TextBox textSoNguyenTo;
+        private System.Windows.Forms.TextBox textSoChinhPhuong;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Ex3_week6_NguyenHuuHoang/Ex3_week6_NguyenHuuHoang/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex3_week6_NguyenHuuHoang
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textn.Clear();
+            textSoNguyenTo.Clear();
+            textSoChinhPhuong.Clear();
+            textn.Focus();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textn.Text);
+            if ((textn.Text).Equals(""))
+                MessageBox.Show("Bạn chưa nhập n. Vui lòng nhập n");
+            else
+            {
+                // in số nguyên tố
+                for (int i = 2; i <= n; i++)
+                {
+                    bool kt = true;
+                    for (int j = 2; j * j <= i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            kt = false;
+                            break;
+                        }    
+                    }    
+                    if (kt == true && i != 1)
+                    {
+                        textSoNguyenTo.Text += i.ToString() + ", ";
+                    }    
+                }  
+                // in số chính phương
+                for (int i = 1; i <= n; i++)
+                {
+                    int sqrt = (int)Math.Sqrt(i);
+                    if (Math.Pow(sqrt,2) == i)
+                    {
+                        textSoChinhPhuong.Text += i.ToString() + ", ";
+                    }    
+                }    
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
