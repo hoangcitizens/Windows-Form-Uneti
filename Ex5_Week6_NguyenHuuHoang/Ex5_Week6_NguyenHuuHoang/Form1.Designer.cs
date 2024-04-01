7f475452afeea7fa50c7e2993b8027cb58475e3b
SAM <hoanga9k38@gmail.com>
2024-04-01 21:22:03 +0700

    first commit

---
+++ Ex5_Week6_NguyenHuuHoang/Ex5_Week6_NguyenHuuHoang/Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace Ex5_Week6_NguyenHuuHoang
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.text5 = new System.Windows.Forms.TextBox();
+            this.text4 = new System.Windows.Forms.TextBox();
+            this.text3 = new System.Windows.Forms.TextBox();
+            this.text2 = new System.Windows.Forms.TextBox();
+            this.text1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textVitri = new System.Windows.Forms.TextBox();
+            this.textTong = new System.Windows.Forms.TextBox();
+            this.textSoLonNhat = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.text5);
+            this.groupBox1.Controls.Add(this.text4);
+            this.groupBox1.Controls.Add(this.text3);
+            this.groupBox1.Controls.Add(this.text2);
+            this.groupBox1.Controls.Add(this.text1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(89, 40);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(626, 185);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(351, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(158, 54);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Thực hiện";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(137, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(158, 54);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Nhập";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // text5
+            // 
+            this.text5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text5.Location = new System.Drawing.Point(514, 21);
+            this.text5.Multiline = true;
+            this.text5.Name = "text5";
+            this.text5.ReadOnly = true;
+            this.text5.Size = new System.Drawing.Size(66, 43);
+            this.text5.TabIndex = 5;
+            // 
+            // text4
+            // 
+            this.text4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text4.Location = new System.Drawing.Point(420, 21);
+            this.text4.Multiline = true;
+            this.text4.Name = "text4";
+            this.text4.ReadOnly = true;
+            this.text4.Size = new System.Drawing.Size(66, 43);
+            this.text4.TabIndex = 4;
+            // 
+            // text3
+            // 
+            this.text3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text3.Location = new System.Drawing.Point(320, 21);
+            this.text3.Multiline = true;
+            this.text3.Name = "text3";
+            this.text3.ReadOnly = true;
+            this.text3.Size = new System.Drawing.Size(66, 43);
+            this.text3.TabIndex = 3;
+            // 
+            // text2
+            // 
+            this.text2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text2.Location = new System.Drawing.Point(220, 21);
+            this.text2.Multiline = true;
+            this.text2.Name = "text2";
+            this.text2.ReadOnly = true;
+            this.text2.Size = new System.Drawing.Size(66, 43);
+            this.text2.TabIndex = 2;
+            // 
+            // text1
+            // 
+            this.text1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text1.Location = new System.Drawing.Point(119, 21);
+            this.text1.Multiline = true;
+            this.text1.Name = "text1";
+            this.text1.ReadOnly = true;
+            this.text1.Size = new System.Drawing.Size(66, 43);
+            this.text1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(15, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mảng số:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textVitri);
+            this.groupBox2.Controls.Add(this.textTong);
+            this.groupBox2.Controls.Add(this.textSoLonNhat);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.ForeColor = System.Drawing.Color.Blue;
+            this.groupBox2.Location = new System.Drawing.Point(89, 256);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(625, 162);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "KẾT QUẢ";
+            // 
+            // textVitri
+            // 
+            this.textVitri.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textVitri.Location = new System.Drawing.Point(494, 47);
+            this.textVitri.Multiline = true;
+            this.textVitri.Name = "textVitri";
+            this.textVitri.ReadOnly = true;
+            this.textVitri.Size = new System.Drawing.Size(112, 48);
+            this.textVitri.TabIndex = 5;
+            // 
+            // textTong
+            // 
+            this.textTong.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textTong.Location = new System.Drawing.Point(204, 105);
+            this.textTong.Multiline = true;
+            this.textTong.Name = "textTong";
+            this.textTong.ReadOnly = true;
+            this.textTong.Size = new System.Drawing.Size(148, 42);
+            this.textTong.TabIndex = 4;
+            // 
+            // textSoLonNhat
+            // 
+            this.textSoLonNhat.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textSoLonNhat.Location = new System.Drawing.Point(144, 47);
+            this.textSoLonNhat.Multiline = true;
+            this.textSoLonNhat.Name = "textSoLonNhat";
+            this.textSoLonNhat.ReadOnly = true;
+            this.textSoLonNhat.Size = new System.Drawing.Size(151, 42);
+            this.textSoLonNhat.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(430, 59);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 22);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Vị trí:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(15, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(172, 22);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Tổng các phần tử:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(15, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 22);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Số lớn nhất:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MINH HOA VE MANG";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox text5;
+        private System.Windows.Forms.TextBox text4;
+        private System.Windows.Forms.TextBox text3;
+        private System.Windows.Forms.TextBox text2;
+        private System.Windows.Forms.TextBox text1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textVitri;
+        private System.Windows.Forms.TextBox textTong;
+        private System.Windows.Forms.TextBox textSoLonNhat;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Ex5_Week6_NguyenHuuHoang/Ex5_Week6_NguyenHuuHoang/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
+
+namespace Ex5_Week6_NguyenHuuHoang
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // Viết thủ tục Nhapmang() thực hiện lưu 5 số nguyên ngẫu nhiên từ 1 đến 100 
+        private int[] a = new int[5];
+        private Random random = new Random();
+        private void nhapMang()
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = random.Next(1,101);
+            }    
+        }
+        // Thủ tục xuấtmang() gán 5 giá trị từ mảng vào 5 Textbox 
+        private void xuatMang()
+        {
+            text1.Text = a[0].ToString();
+            text2.Text = a[1].ToString();
+            text3.Text = a[2].ToString();
+            text4.Text = a[3].ToString();
+            text5.Text = a[4].ToString();
+        }
+
+        // Sự kiện Click của nút Nhập gọi 2 thủ tục trên
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nhapMang();
+            xuatMang();
+            textSoLonNhat.Clear();
+            textTong.Clear();
+            textVitri.Clear();
+        }
+        // Thủ tục Timmax() để Tìm giá trị lơn nhất và vị trí 
+        private void timMax()
+        {
+            int max = a[0];
+            int pos = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] > max)
+                {
+                    max = a[i];
+                    pos = i + 1;
+                }    
+            }    
+            textSoLonNhat.Text = max.ToString();
+            textVitri.Text = pos.ToString();
+        }
+        // Viết hàm Tong() để tính tổng các phần tử trong mảng 
+        private void tinhTong()
+        {
+            int S = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                S += a[i];
+            }    
+            textTong.Text = S.ToString();
+        }
+        // Sự kiện Click của nút thực hiện Gọi thủ tục Timmax() và hàm Tong() 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timMax();
+            tinhTong();
+        }
+    }
+}
